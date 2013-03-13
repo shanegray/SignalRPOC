2f99694e9cd6761ca5cf99ff5e812576fdf5ee9f
Shane Gray <shane@drimroe.com>
2013-03-13 13:02:30 +0000

    Initial Commit
    Can't download the SignalR packages yet

---
+++ source/LogMonitor/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LogMonitor
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Main",
+                url: "",
+                defaults: new { controller = "Main", action = "Index" }
+            );
+        }
+    }
+}
---
+++ source/LogMonitor/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace LogMonitor.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
